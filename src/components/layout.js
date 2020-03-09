cad44eb5e8ed81b6e358d95d1fc8605b9fa3addb
Vicente Canales <vicente@canales.io>
2020-03-08 22:25:29 -0300

    fix header link

--- src/components/layout.js
+++ src/components/layout.js
@@ -26,7 +26,7 @@ class Layout extends React.Component {
               textDecoration: `none`,
               color: `inherit`,
             }}
-            to={location.pathname === blogPath ? `/blog/` : `/`}
+            to={`/`}
           >
             {title}
           </Link>
