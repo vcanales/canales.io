f492c6dfd7ada7dfc9dcc91efff43073832c74b5
Vicente Canales <vicente@canales.io>
2020-03-08 22:20:06 -0300

    make the index the list of posts

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,35 +1,77 @@
 import React from "react"
-import { Link } from "gatsby"
+import { Link, graphql } from "gatsby"
 
+import Bio from "../components/bio"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import { rhythm } from "../utils/typography"
 import Button from "../components/button"
 
-class IndexPage extends React.Component {
+class Blog extends React.Component {
   render() {
-    const siteTitle = "Vicente Canales"
+    const { data } = this.props
+    const siteTitle = data.site.siteMetadata.title
+    const posts = data.allMdx.edges
 
     return (
       <Layout location={this.props.location} title={siteTitle}>
-        <SEO
-          title="Home"
-          keywords={[
-            `blog`,
-            `vicente canales`,
-            `javascript`,
-            `react`,
-            `node`,
-            `programming`,
-            `consulting`,
-          ]}
-        />
-        <h1>Hi everyone!</h1>
-        <Link to="/blog/">
-          <Button marginTop="35px">Go to Blog</Button>
-        </Link>
+        <SEO title="All posts" />
+        <Bio />
+        <div style={{ margin: "20px 0 40px" }}>
+          {posts.map(({ node }) => {
+            const title = node.frontmatter.title || node.fields.slug
+            return (
+              <div key={node.fields.slug}>
+                <h3
+                  style={{
+                    marginBottom: rhythm(1 / 4),
+                  }}
+                >
+                  <Link
+                    style={{ boxShadow: `none` }}
+                    to={`blog${node.fields.slug}`}
+                  >
+                    {title}
+                  </Link>
+                </h3>
+                <small>{node.frontmatter.date}</small>
+                <p
+                  dangerouslySetInnerHTML={{
+                    __html: node.frontmatter.description || node.excerpt,
+                  }}
+                />
+              </div>
+            )
+          })}
+        </div>
       </Layout>
     )
   }
 }
 
-export default IndexPage
+export default Blog
+
+export const pageQuery = graphql`
+  query {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+    allMdx(sort: { fields: [frontmatter___date], order: DESC }) {
+      edges {
+        node {
+          excerpt
+          fields {
+            slug
+          }
+          frontmatter {
+            date(formatString: "MMMM DD, YYYY")
+            title
+            description
+          }
+        }
+      }
+    }
+  }
+`
